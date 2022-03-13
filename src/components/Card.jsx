d6ac6c98354c4be84d281f246b17e9a687e460ea
marvin obig <obig.marv@gmail.com>
2022-03-13 15:38:49 +0000

    created nav and card components

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+export default function Card(props) {
+    return (
+        <section className="card-container">
+            <div className="image">
+                <img src ={`../media/${props.img}`} alt="location" />
+            </div>
+            <div className="location-info">
+                <div className="maps">
+                    <i className="material-icons">room</i>
+                    <p>{props.country}</p>
+                    <a href={props.map} alt="google map link to location" target="_blank">View in maps</a>
+                </div>
+                <h1 className="title">{props.location}</h1>
+                <h4 className="date-of-travel">{props.date}</h4>
+                <p className="description">
+                    {props.description}
+                </p>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+export default function Nav() {
+    return (
+        <nav>
+            <i className="material-icons">public</i>
+            <h1>Travel Journal</h1>
+        </nav>
+    )
+}
