fbae162a772e289536c6a3552bff484f62f0f0e1
marvin obig <obig.marv@gmail.com>
2022-03-13 15:41:11 +0000

    added rel="noreferrer" to map link in card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ export default function Card(props) {
                 <div className="maps">
                     <i className="material-icons">room</i>
                     <p>{props.country}</p>
-                    <a href={props.map} alt="google map link to location" target="_blank">View in maps</a>
+                    <a href={props.map} alt="google map link to location" rel="noreferrer" target="_blank">View in maps</a>
                 </div>
                 <h1 className="title">{props.location}</h1>
                 <h4 className="date-of-travel">{props.date}</h4>
