f7ba4bd37b1a7fe18958b9bf9b3d92e3d57adc99
marvin obig <obig.marv@gmail.com>
2022-03-13 15:39:12 +0000

    imported components into main app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import Nav from './components/Nav';
+import { CARD_DATA } from './TravelData';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-     
+     <Nav />
+     {CARD_DATA}
     </div>
   );
 }
